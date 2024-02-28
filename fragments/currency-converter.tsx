aa8228760fc83d62b4328990c8c6c1616f499985
ArturMichalak <arturmichalak96@gmail.com>
2024-02-28 16:15:40 +0100

    state of converter parameters

--- fragments/currency-converter.tsx
+++ fragments/currency-converter.tsx
@@ -5,8 +5,8 @@ import { useExchange, useFetch } from '@/hooks';
 import { getCountryCodes, type ExchangeApiResult } from '@/utils';
 import { MenuItem } from '@mui/material';
 import Select, { type SelectChangeEvent } from '@mui/material/Select';
-import TextField, { StandardTextFieldProps } from '@mui/material/TextField';
-import { useMemo, useReducer } from 'react';
+import TextField, { type TextFieldProps } from '@mui/material/TextField';
+import { ChangeEventHandler, useMemo, useReducer } from 'react';
 
 interface CurrencyConverterProps {
   fetchUrl: string;
@@ -26,23 +26,29 @@ export default function CurrencyConverter({
   function onFromChange(event: SelectChangeEvent) {
     if (data === null) return;
     const value = data.rates[event.target.value];
-    changeCurrency(value, true)
+    changeCurrency(value, true);
   }
 
   function onToChange(event: SelectChangeEvent) {
     if (data === null) return;
     const value = data.rates[event.target.value];
-    changeCurrency(value, false)
+    changeCurrency(value, false);
   }
 
-  function onAmountChange(event: StandardTextFieldProps) {
+  function onAmountChange(
+    event: Parameters<ChangeEventHandler<HTMLInputElement>>[0],
+  ) {
     if (data === null) return;
-    changeCurrency(0, false)
+    const value = parseInt(event.target.value);
+    changeCurrency(value, true);
   }
 
-  function onConvertedAmountChange(event: StandardTextFieldProps) {
+  function onConvertedAmountChange(
+    event: Parameters<ChangeEventHandler<HTMLInputElement>>['0'],
+  ) {
     if (data === null) return;
-    changeCurrency(0, false)
+    const value = parseInt(event.target.value);
+    changeCurrency(value, false);
   }
 
   if (loading || data === null) return 'Loading...';
@@ -60,20 +66,24 @@ export default function CurrencyConverter({
             ))}
           </Select>
 
-          <TextField defaultValue={0} type='number' onChange={onAmountChange} />
+          <TextField defaultValue={0} type="number" onChange={onAmountChange} />
         </div>
       </div>
       <div>
         Converted Amount
         <div>
-        <Select defaultValue="PLN" onChange={onToChange}>
+          <Select defaultValue="PLN" onChange={onToChange}>
             {countryCodes!.map((code) => (
               <MenuItem key={code} value={code}>
                 {code}
               </MenuItem>
             ))}
           </Select>
-          <TextField defaultValue={0} type='number' onChange={onConvertedAmountChange} />
+          <TextField
+            defaultValue={0}
+            type="number"
+            onChange={onConvertedAmountChange}
+          />
         </div>
       </div>
     </Card>
