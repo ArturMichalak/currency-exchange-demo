4751638798f60ade71ed1f1c892af90f34c465fc
ArturMichalak <arturmichalak96@gmail.com>
2024-02-28 12:09:07 +0100

    add webServer config in tests env configuration

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('e2e', () => {
   test('is the main page the same?', async ({ page }) => {
-    await page.goto('127.0.0.1:3000/');
+    await page.goto('http://127.0.0.1:3000/');
     await expect(page).toHaveScreenshot();
   });
 });
